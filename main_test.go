6b34c826dba2ec9571ce07ce66bd8fba6f0a9cd9
yesimar <yesitrincado24@gmail.com>
2025-04-03 15:57:27 +0200

    parte 6 hacer que el test falle

--- main_test.go
+++ main_test.go
@@ -38,6 +38,6 @@ func TestAdditionHandler(t *testing.T) {
 
 }
 
-func Testfalla(t *testing.T) {
-	t.Error("Este test falla con toda la intención")
+func Testfail(t *testing.T) {
+	t.FailNow("Este test falla con toda la intención")
 }
